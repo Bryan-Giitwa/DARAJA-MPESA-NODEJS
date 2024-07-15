155269f18aa9ff9419d296dc7b499fd7cd728e4c
Bryan-Giitwa <giitwab4@gmail.com>
2024-07-15 18:23:59 +0300

    adding new data

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Routes
 app.use("/api", routes);
 
 app.listen(process.env.PORT, () => {
--- routes/lipanampesa.js
+++ routes/lipanampesa.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const getToken = require("../middlewares/generateAccessToken");
 const { intiateSTKPush, stkCallback } = require("../controllers/lipanampesa");
 
+//Route to test if the server is working
 router.get("/", (req, res) => {
   res.send("Hello From Daraja Api");
 });
 
-router.post("/stk-push", getToken, intiateSTKPush);
-router.post("/stkCallback", stkCallback);
+router.post("/stk-push", getToken, intiateSTKPush); //intiateSTKPush will be called after getToken middleware
+router.post("/stkCallback", stkCallback); //Route to get the callbac data from safaricom
 
 module.exports = router;
